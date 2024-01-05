141b24956ffcda759a72dbef15e59ddd7d0ae94d
chiku <vipinsharma2909@gmail.com>
2024-01-05 16:33:18 +0530

    Implemented Next Permutation Using simple techinique

---
+++ arrays-3-interview-problems/next__permutation.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) (int)((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for(int i = 0; i < (int)(n); i++)
+#define for1(i, n) for(int i = 1; i <= (int)(n); i++)
+#define ford(i, n) for(int i = (int)(n) - 1; i >= 0; i -= 1)
+#define fore(i, a, b) for(int i = (int)(a); i <= (int)(b); i++)
+#define LOCAL_DEFINE 
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long i64;
+typedef vector<i64> vi64;
+typedef vector<vi64> vvi64;
+typedef pair<i64, i64> pi64;
+typedef double ld;
+
+template<class T> bool uni(T &a, T b) { return a > b ? (a = b, true) : false;}
+template<class T> bool uax(T &a, T b) { return a < b ? (a = b, true) : false;}
+
+const i64 P = 1000000000 + 7;
+
+vi solve(vi &A) {
+    int n = sz(A);
+    int i, j;
+    for(i = n-2; i >= 0; i -= 1) if(A[i] < A[i+1]) break;
+    if(i == -1) {
+        reverse(all(A));
+        return A;
+    }
+    for(j = n-1; j >= 0; j -= 1) if(A[j] > A[i]) break;
+    swap(A[i], A[j]);
+    reverse(A.begin()+i+1, A.end());
+    return A;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+    cout << fixed;
+// #ifdef LOCAL_DEFINE
+//     freopen("input.txt", "rt", stdin);
+// #endif
+
+    int n;
+    cin >> n;
+    vi A(n);
+    forn(i, n) cin >> A[i];
+    solve(A);
+    forn(i, n) cout << A[i] << " ";
+    cout << "\n";
+
+
+#ifdef LOCAL_DEFINE
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+    return 0;
+}
+
