3305d3571a5a10b97c1ebc10e67c77beeaab0331
chiku <vipinsharma2909@gmail.com>
2024-01-10 21:26:19 +0530

    Implemented Number of door opens using CRUX only squared number of doors will only be opened

---
+++ prime-numbers/number_of_open_doors.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int solve(int n) {
+    int i = 1, cnt = 0;
+    while(i*i <= n) {
+        cnt += 1;
+        i += 1;
+    }
+    return cnt;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int res = solve(A);
+    cout << "Result: " << res << '\n';
+    return 0;
+}
