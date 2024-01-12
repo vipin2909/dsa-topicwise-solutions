cef634b630193ca2cdd7f8e7d323099027887e8b
chiku <vipinsharma2909@gmail.com>
2024-01-13 00:39:16 +0530

    Make Changes by Testing this (Insert element in an sorted array)

--- binary-search-1/SortedInsertPosition.java
+++ binary-search-1/SortedInsertPosition.java
@@ -12,5 +12,11 @@ public class SortedInsertPosition {
             }
         }
         return lo;
-    }    
+    }   
+    
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        int ind = new SortedInsertPosition().solve(arr, 8);
+        System.out.println("Element Inserting At: " + ind);
+    }
 }
