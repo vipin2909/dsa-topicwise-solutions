0e7dd41bed88c92d2b827906c44be27f507715fa
chiku <vipinsharma2909@gmail.com>
2024-01-15 10:58:02 +0530

    Added another implementation for finding element which occured only once

--- binary-search-1/SingleElement.java
+++ binary-search-1/SingleElement.java
@@ -1,5 +1,25 @@
 
 public class SingleElement {
+
+    // modified solution 
+    public int singleElement(int[] A) {
+        int ans = 0, N = A.length;
+        int lo = 0, hi = N - 1;
+
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            // if mid reached last position it means all the elements are twice repeted
+            if(mid == N-1) return A[N-1];
+            if(A[mid] == A[mid+1]) mid += 1;
+            if(mid % 2 == 1) lo = mid + 1;
+            else {
+                ans = mid;
+                hi = mid - 1;
+            }
+        }
+
+        return ans;
+    }
     public int solve(int[] A) {
         int ans = 0;
         int lo = 0, N = A.length, hi = A.length - 1;
